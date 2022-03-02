eb2506891a29ef7f5bf42a4baf94d1edac7f632b
bennis yiu <bennisyiu@gmail.com>
2022-03-02 23:23:49 +0800

    quick youtube clone

---
+++ src/components/search_bar.js
@@ -0,0 +1,33 @@
+import { divide } from 'lodash';
+import React, { Component } from 'react';
+
+// const SearchBar = () => {
+//   return <input />;
+// };
+
+// defining class component
+class SearchBar extends Component {
+// setting state
+constructor(props) {
+  super(props);
+
+  this.state = { term: 'Search' };
+}
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={ event => this.onInputChange(event.target.value) } />
+      </div>
+    )
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyC9AwR2eIr0175bx39JI7TaGdP9fU4m5sc';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state = {
+      videos: [],
+      seclectedVideo: null
+    };
+
+    this.videoSearch('hello');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
+      // this.setState({ videos: videos }) **if the key and variable have the same name
+    });
+  }
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300) // 300 miliseconds
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
